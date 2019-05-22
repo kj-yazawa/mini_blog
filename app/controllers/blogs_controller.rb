b3fb2af75c96e6cf94d7258e5aec765fb9f8fb98
Kojiro Yazawa <yazawa17ko@gmail.com>
2019-05-23 06:46:06 +0900

    Add create new blog function

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   def new
+    @blog = Blog.new
   end
 
   def show
@@ -10,5 +11,20 @@ class BlogsController < ApplicationController
 
   def index
     @blogs = Blog.all
+  end
+
+  def create
+    blog = Blog.new(blog_params)
+    if blog.save
+      redirect_to blogs_url, notice: "ブログ「#{blog.title}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title)
   end
 end
